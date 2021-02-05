9fdb6e75b1909e10f8f56dd1c6fbe53ab9e76166
Chad <chadoliver017@gmail.com>
2021-02-05 13:51:47 -0500

    updated cloud function & parameters refactored to use polygon & circle (drawing still there, just commented out)

--- lib/screens/drawable_map.dart
+++ lib/screens/drawable_map.dart
@@ -36,30 +36,253 @@ class _MapPageState extends State<MapPage> {
   bool _clearDrawing = false;
   int _lastXCoordinate, _lastYCoordinate;
 
+  Set<Marker> _markers = HashSet<Marker>();
+  Set<Polygon> _polygons2 = HashSet<Polygon>();
+  Set<Circle> _circles = HashSet<Circle>();
+  List<LatLng> _polygonLatLngs = [];
+  double radius;
+  GoogleMapController _googleMapController;
+  BitmapDescriptor _markerIcon;
+  //ids
+  int _polygonIdCounter = 1;
+  int _circleIdCounter = 1;
+  int _markerIdCounter = 1;
+
+  // Type controllers
+  bool _isPolygon = true; //Default
+  bool _isMarker = false;
+  bool _isCircle = false;
+
+  // This function is to change the marker icon
+  void _setMarkerIcon() async {
+    _markerIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), 'assets/farm.png');
+  }
+
+  // Draw Polygon to the map
+  void _setPolygon() {
+    final String polygonIdVal = 'polygon_id_$_polygonIdCounter';
+    _polygons.add(Polygon(
+      polygonId: PolygonId(polygonIdVal),
+      points: _polygonLatLngs,
+      strokeWidth: 2,
+      strokeColor: Colors.yellow,
+      fillColor: Colors.yellow.withOpacity(0.15),
+    ));
+  }
+
+  // Set circles as points to the map
+  void _setCircles(LatLng point) {
+    final String circleIdVal = 'circle_id_$_circleIdCounter';
+    _circleIdCounter++;
+    print(
+        'Circle | Latitude: ${point.latitude}  Longitude: ${point.longitude}  Radius: $radius');
+    _circles.add(Circle(
+        circleId: CircleId(circleIdVal),
+        center: point,
+        radius: radius,
+        fillColor: Colors.redAccent.withOpacity(0.5),
+        strokeWidth: 3,
+        strokeColor: Colors.redAccent));
+  }
+
+  // Set Markers to the map
+  void _setMarkers(LatLng point) {
+    final String markerIdVal = 'marker_id_$_markerIdCounter';
+    _markerIdCounter++;
+    setState(() {
+      print(
+          'Marker | Latitude: ${point.latitude}  Longitude: ${point.longitude}');
+      _markers.add(
+        Marker(
+          markerId: MarkerId(markerIdVal),
+          position: point,
+        ),
+      );
+    });
+  }
+
+  // Start the map with this marker setted up
+  void _onMapCreated(GoogleMapController controller) {
+    _googleMapController = controller;
+
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('0'),
+          position: LatLng(-20.131886, -47.484488),
+          infoWindow:
+              InfoWindow(title: 'Roça', snippet: 'Um bom lugar para estar'),
+          //icon: _markerIcon,
+        ),
+      );
+    });
+  }
+
+  Widget _fabPolygon() {
+    return FloatingActionButton.extended(
+      onPressed: () {
+        //Remove the last point setted at the polygon
+        setState(() {
+          _polygonLatLngs.removeLast();
+        });
+      },
+      icon: Icon(Icons.undo),
+      label: Text('Undo point'),
+      backgroundColor: Colors.orange,
+    );
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: GestureDetector(
+  //       onPanUpdate: (_drawPolygonEnabled) ? _onPanUpdate : null,
+  //       onPanEnd: (_drawPolygonEnabled) ? _onPanEnd : null,
+  //       child: GoogleMap(
+  //         mapType: MapType.normal,
+  //         initialCameraPosition: _kGooglePlex,
+  //         polygons: _polygons,
+  //         polylines: _polyLines,
+  //         onMapCreated: (GoogleMapController controller) {
+  //           _controller.complete(controller);
+  //         },
+  //       ),
+  //     ),
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: _toggleDrawing,
+  //       tooltip: 'Drawing',
+  //       child: Icon((_drawPolygonEnabled) ? Icons.cancel : Icons.edit),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onPanUpdate: (_drawPolygonEnabled) ? _onPanUpdate : null,
-        onPanEnd: (_drawPolygonEnabled) ? _onPanEnd : null,
-        child: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: _kGooglePlex,
-          polygons: _polygons,
-          polylines: _polyLines,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
+        appBar: AppBar(
+          title: Text('Set Digital Parameters'),
+          centerTitle: true,
         ),
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.startFloat
-      // FloatingActionButtonLocation()
-      // FloatingActionButton(
-      //   onPressed: _toggleDrawing,
-      //   tooltip: 'Drawing',
-      //   child: Icon((_drawPolygonEnabled) ? Icons.cancel : Icons.edit),
-      // ),
-    );
+        floatingActionButton:
+            _polygonLatLngs.length > 0 && _isPolygon ? _fabPolygon() : null,
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              initialCameraPosition: _kGooglePlex,
+              mapType: MapType.hybrid,
+              markers: _markers,
+              circles: _circles,
+              polygons: _polygons,
+              myLocationEnabled: true,
+              onTap: (point) {
+                if (_isPolygon) {
+                  setState(() {
+                    _polygonLatLngs.add(point);
+                    _setPolygon();
+                  });
+                } else if (_isMarker) {
+                  setState(() {
+                    _markers.clear();
+                    _setMarkers(point);
+                  });
+                } else if (_isCircle) {
+                  setState(() {
+                    _circles.clear();
+                    _setCircles(point);
+                  });
+                }
+              },
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Row(
+                children: <Widget>[
+                  RaisedButton(
+                      color: Colors.black54,
+                      onPressed: () {
+                        _isPolygon = true;
+                        _isMarker = false;
+                        _isCircle = false;
+                      },
+                      child: Text(
+                        'Polygon',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                      )),
+                  // RaisedButton(
+                  //     color: Colors.black54,
+                  //     onPressed: () {
+                  //       _isPolygon = false;
+                  //       _isMarker = true;
+                  //       _isCircle = false;
+                  //     },
+                  //     child: Text('Marker',
+                  //         style: TextStyle(
+                  //             fontWeight: FontWeight.bold,
+                  //             color: Colors.white))),
+                  RaisedButton(
+                      color: Colors.black54,
+                      onPressed: () {
+                        _isPolygon = false;
+                        _isMarker = false;
+                        _isCircle = true;
+                        radius = 50;
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              child:
+                              return AlertDialog(
+                                backgroundColor: Colors.grey[900],
+                                title: Text(
+                                  'Choose the radius (in meters)',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                content: Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Material(
+                                      color: Colors.black,
+                                      child: TextField(
+                                        style: TextStyle(
+                                            fontSize: 16, color: Colors.white),
+                                        decoration: InputDecoration(
+                                          icon: Icon(Icons.zoom_out_map),
+                                          hintText: 'Ex: 100',
+                                          suffixText: 'meters',
+                                        ),
+                                        keyboardType:
+                                            TextInputType.numberWithOptions(),
+                                        onChanged: (input) {
+                                          setState(() {
+                                            radius = double.parse(input);
+                                          });
+                                        },
+                                      ),
+                                    )),
+                                actions: <Widget>[
+                                  FlatButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: Text(
+                                        'Ok',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      )),
+                                ],
+                              );
+                            });
+                      },
+                      child: Text('Circle',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)))
+                ],
+              ),
+            )
+          ],
+        ));
   }
 
   _toggleDrawing() {
