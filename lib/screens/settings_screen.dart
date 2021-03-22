704d8eba70478c579d436f0267b11e7ca3e4e8f9
Chad <chadoliver017@gmail.com>
2021-03-22 12:42:23 -0500

    Settings screen refactored to use futurebuilder

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -10,6 +10,7 @@ import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../helpers/auth_helper.dart';
 
 class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -46,6 +47,8 @@ class _SettingsScreenState extends State<Settings> {
   void fetchData() async {
     user = _auth.currentUser;
     uid = user.uid;
+
+    var data = await AuthHelper.fetchData();
     FirebaseFirestore.instance
         .collection('users')
         .doc(uid)
@@ -62,133 +65,159 @@ class _SettingsScreenState extends State<Settings> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          brightness: Brightness.light,
-          // iconTheme: IconThemeData(color: Colors.white),
-          backgroundColor: Colors.black,
-          title: Text(
-            'Settings',
-            style: kLabelStyle2.copyWith(color: Colors.white),
-          ),
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 30.0),
-              Row(
-                children: <Widget>[
-                  Container(
-                    width: 85,
-                    height: 85,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: photoLink != null
-                            ? NetworkImage(photoLink)
-                            : AssetImage("assets/images/profile.png"),
-                        fit: BoxFit.cover,
-                      ),
-                      // border: Border.all(color: Colors.lightBlueAccent),
-                    ),
+    return FutureBuilder(
+        future: AuthHelper.fetchData(),
+        builder: (ctx, futureSnapshot) {
+          if (futureSnapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  brightness: Brightness.light,
+                  // iconTheme: IconThemeData(color: Colors.white),
+                  backgroundColor: Colors.black,
+                  title: Text(
+                    'Settings',
+                    style: kLabelStyle2.copyWith(color: Colors.white),
                   ),
-                  SizedBox(width: 10.0),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ));
+          }
+
+          if (futureSnapshot.hasData) {
+            final userData = futureSnapshot.data;
+            print("PHOTO LINK:" + userData.photoLink);
+            return Scaffold(
+              appBar: AppBar(
+                automaticallyImplyLeading: false,
+                brightness: Brightness.light,
+                // iconTheme: IconThemeData(color: Colors.white),
+                backgroundColor: Colors.black,
+                title: Text(
+                  'Settings',
+                  style: kLabelStyle2.copyWith(color: Colors.white),
+                ),
+              ),
+              body: SingleChildScrollView(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(height: 30.0),
+                    Row(
                       children: <Widget>[
-                        Text(
-                          "$firstName $lastName",
-                          style: GoogleFonts.ubuntu().copyWith(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
+                        Container(
+                          width: 85,
+                          height: 85,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: photoLink != null
+                                  ? NetworkImage(userData.photoLink)
+                                  : AssetImage("assets/images/profile.png"),
+                              fit: BoxFit.cover,
+                            ),
+                            // border: Border.all(color: Colors.lightBlueAccent),
                           ),
                         ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "$parish, Jamaica ${Emojis.flagJamaica}",
-                              style: GoogleFonts.abel().copyWith(),
-                            ),
-                            // Flag(
-                            //   'JM',
-                            //   height: 10,
-                            //   width: 10,
-                            //   fit: BoxFit.fill,
-                            // ),
-                          ],
+                        SizedBox(width: 10.0),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "${userData.firstName} ${userData.lastName}",
+                                style: GoogleFonts.ubuntu().copyWith(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "${userData.parish}, Jamaica ${Emojis.flagJamaica}",
+                                    style: GoogleFonts.abel().copyWith(),
+                                  ),
+                                  // Flag(
+                                  //   'JM',
+                                  //   height: 10,
+                                  //   width: 10,
+                                  //   fit: BoxFit.fill,
+                                  // ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              ListTile(
-                title: Text("Notification Settings",
-                    style: GoogleFonts.sarala().copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    )),
-                trailing: Icon(
-                  Icons.logout,
-                  color: Colors.black,
+                    SizedBox(height: 20.0),
+                    ListTile(
+                      title: Text("Notification Settings",
+                          style: GoogleFonts.sarala().copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          )),
+                      trailing: Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      onTap: () {
+                        // FirebaseAuth.instance.signOut();
+                        // Navigator.pushNamed(context, '/login');
+                      },
+                    ),
+                    ListTile(
+                      title: Text("Reset Password",
+                          style: GoogleFonts.sarala().copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          )),
+                      trailing: Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      onTap: () {
+                        // FirebaseAuth.instance.signOut();
+                        // Navigator.pushNamed(context, '/login');
+                      },
+                    ),
+                    ListTile(
+                      title: Text("Help",
+                          style: GoogleFonts.sarala().copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          )),
+                      trailing: Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      onTap: () {
+                        // FirebaseAuth.instance.signOut();
+                        // Navigator.pushNamed(context, '/login');
+                      },
+                    ),
+                    ListTile(
+                      title: Text("Log Out",
+                          style: GoogleFonts.sarala().copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          )),
+                      trailing: Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      onTap: () {
+                        FirebaseAuth.instance.signOut();
+                        Navigator.pushNamed(context, '/login');
+                      },
+                    ),
+                  ],
                 ),
-                onTap: () {
-                  // FirebaseAuth.instance.signOut();
-                  // Navigator.pushNamed(context, '/login');
-                },
               ),
-              ListTile(
-                title: Text("Reset Password",
-                    style: GoogleFonts.sarala().copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    )),
-                trailing: Icon(
-                  Icons.logout,
-                  color: Colors.black,
-                ),
-                onTap: () {
-                  // FirebaseAuth.instance.signOut();
-                  // Navigator.pushNamed(context, '/login');
-                },
-              ),
-              ListTile(
-                title: Text("Help",
-                    style: GoogleFonts.sarala().copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    )),
-                trailing: Icon(
-                  Icons.logout,
-                  color: Colors.black,
-                ),
-                onTap: () {
-                  // FirebaseAuth.instance.signOut();
-                  // Navigator.pushNamed(context, '/login');
-                },
-              ),
-              ListTile(
-                title: Text("Log Out",
-                    style: GoogleFonts.sarala().copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    )),
-                trailing: Icon(
-                  Icons.logout,
-                  color: Colors.black,
-                ),
-                onTap: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pushNamed(context, '/login');
-                },
-              ),
-            ],
-          ),
-        ));
+            );
+          }
+        });
   }
 }
